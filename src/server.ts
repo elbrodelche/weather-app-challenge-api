d784b259ebb492bef3d3607f6e3d4a4a261d98f4
Juan Carlos Migliavacca <jcmigliavacca@gmail.com>
2020-07-14 17:18:10 -0300

    Server structure, and entrypoint.

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import  * as Koa from 'koa';
+import * as logger from 'koa-logger'
+import * as Router from 'koa-router';
+
+// Koa instances
+const app = new Koa();
+const router = new  Router;
+
+// Middleware
+app.use(logger());
+app.use(router.routes());
+
+// Listening port
+app.listen(3000)
+
+// Success Message
+console.log('Up and running on port 3000')
