a22bae365aad050f83fee31659497e2cf7bc9928
Juan Carlos Migliavacca <jcmigliavacca@gmail.com>
2020-07-14 20:03:23 -0300

    Add env files config.

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import  * as Koa from 'koa';
-import * as logger from 'koa-logger';
 import * as Router from 'koa-router';
+import * as logger from 'koa-logger';
+import * as dotenv from 'dotenv';
 import AppRouter from './routes/app.router';
 
 // Koa instances
@@ -8,6 +9,7 @@ const app = new Koa();
 const router = new  Router;
 
 // Middleware
+dotenv.config();
 app.use(logger());
 app.use(router.routes());
 app.use(AppRouter.routes());
